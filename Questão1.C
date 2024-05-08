3876e6f568a8b44357980d19468697447e2fbb63
Cleber Lucas Farias <138718686+CosmoLucs@users.noreply.github.com>
2024-05-08 10:45:20 -0300

    Add files via upload

---
+++ Questão1.C
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define Tam_Max 100
+#define NOME "lucas"
+
+void *lerArquivoThread(void *FileName) {
+    //Usar const pra não haver alteração
+    const char *nomeArquivo = (const char *)FileName;
+
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        pthread_exit(NULL);
+        exit(1);
+    }
+
+    char linha[Tam_Max];
+    int contador = 0;
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+        if (strstr(linha, NOME) != NULL) { 
+            contador++;
+        }
+    }
+
+    // Fecha o arquivo
+    fclose(arquivo);
+
+    pthread_exit((void *) contador);
+}
+
+int main() {
+    const char *arquivos[] = {"Arquivo1.txt", "Arquivo2.txt", "Arquivo3.txt"};
+    pthread_t threads[3];
+    int totalOcorrencias = 0;
+
+    // Cria uma thread para cada arquivo
+    for (int i = 0; i < sizeof(arquivos) / sizeof(arquivos[0]); ++i) {
+        pthread_create(&threads[i], NULL, lerArquivoThread, (void *)arquivos[i]);
+    }
+
+    // Aguarda até que todas as threads tenham terminado e soma as ocorrências
+    for (int i = 0; i < 3); ++i) {
+        int *resultado;
+        pthread_join(threads[i], (void **)&resultado);
+        if (resultado != NULL) {
+            totalOcorrencias += *resultado;
+            free(resultado);
+        }
+    }
+
+    // Printa o total de ocorrências
+    printf("%d\n", totalOcorrencias);
+
+    return 0;
+}
